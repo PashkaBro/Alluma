40e3834aaf3e5f67f7a978be18a3a1a6cb2c0129
Aleksandr Demidov <lira8792@mail.ru>
2020-01-17 13:22:45 -0800

    add martin

--- bla-bla.test.js
+++ bla-bla.test.js
@@ -2,7 +2,8 @@ const users = {
     name1: 'Arthur Senko',
     name2: 'Pasha TheBest',
     name3: 'Svetlana',
-    name4: 'Alex'
+    name4: 'Alex', 
+    name5: 'Martin Luther'
 }
 
 console.log(users);
