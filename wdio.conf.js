be7f9e8542d9bb312feca8bfbf7cbc14c242dd1d
PashkaBro <veenoompoltava@gmail.com>
2020-01-28 23:10:43 -0800

    scenario 1

--- wdio.conf.js
+++ wdio.conf.js
@@ -2,7 +2,7 @@ exports.config = {
     runner: 'local',
     path: '/',
     specs: [
-       './test/specs/all-players-page-for-moderator.test.js'       
+       './test/specs/scenario-game-1.js'       
     ],
     exclude: [
 
